3bc1ae4518516d74a4b2e48e4428cc32fb5ebcac
fufuirdguy <92912808+fufuirdguy@users.noreply.github.com>
2022-01-07 06:36:20 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=c0b3ef90-3402-11ec-8d3d-0242ac130003
+AUUID=4ba9bc7a-e144-4dcb-80c4-548e4b8d800a
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
